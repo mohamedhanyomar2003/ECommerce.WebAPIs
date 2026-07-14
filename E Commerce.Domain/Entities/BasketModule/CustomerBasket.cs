1fcaeddaa2a09d140326187e9ff8625893862f99
compuremo <compuremo@DESKTOP-BIMGJ29>
2026-07-14 07:02:14 +0300

    feat: add CustomerBasket and BasketItem entities for Basket Module

---
+++ E Commerce.Domain/Entities/BasketModule/CustomerBasket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Entities.BasketModule
+{
+    public class CustomerBasket
+    {
+        public int Id { get; set; } = default!; // Guid From Client Side
+        public ICollection<BasketItem> Items { get; set; } = [];
+
+    }
+}
